70a8047f3c082b76d7dbe7e136b5332d87215867
devayanidevayani <devayanidevayani074@gmail.com>
2024-12-17 14:32:07 +0530

    Add files via upload

---
+++ home.txt/may/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for ( i=0;i<=100;i++); do
+touch file.txt
+done
+
+
+
+
---
+++ home.txt/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo  " Enter the number"
+read num
+echo " Enter the number"
+read num
+sum=$(($num+$num))
+echo "sum is $sum"
+
+
+
+
